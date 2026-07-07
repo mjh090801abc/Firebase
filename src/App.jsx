f34e2f1c9a70d71159bd63f1d8579e5b99a39a3e
문정훈 <mjh090801abc@gmail.com>
2026-07-07 21:21:56 +0900

    이메일, 비밀번호를 저장하는 변수 선언(const형)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h1>Firebase 회원가입</h1>
 
-      <input type="email" placeholder="이메일" />
+      <input
+        type="email"
+        placeholder="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
       <br /><br />
 
-      <input type="password" placeholder="비밀번호" />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
       <br /><br />
 
